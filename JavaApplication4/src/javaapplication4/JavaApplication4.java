c5c0f1ac10e26435ca5279f336964db0c0524126
Christqnd <christian-24091992@hotmail.com>
2015-12-02 10:42:50 -0500

    Update JavaApplication4.java

--- JavaApplication4/src/javaapplication4/JavaApplication4.java
+++ JavaApplication4/src/javaapplication4/JavaApplication4.java
@@ -18,7 +18,7 @@ public class JavaApplication4 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        JOptionPane.showMessageDialog(null, "Mensaje de prueba");
+        JOptionPane.showMessageDialog(null, "Mensaje");
     }
     
 }
